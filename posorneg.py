1d3800fdb0b6ce1435bc1eb777474b5ffcfb54eb
ajayg2357 <40806125+ajayg2357@users.noreply.github.com>
2018-07-10 13:17:46 +0530

    Update posorneg.py

--- posorneg.py
+++ posorneg.py
@@ -1,7 +1,7 @@
 a=int(input())
 if (a<0):
-    print("a is  Negative")
+    print("Negative")
 elif(a==0):
-    print("a is Zero")
+    print("Zero")
 elif(a>0):
-    print("a is Positive")
+    print("Positive")
