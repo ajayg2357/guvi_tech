b18384e1c107c9539cec305f2147253dffa6cf79
ajayg2357 <40806125+ajayg2357@users.noreply.github.com>
2018-07-13 09:49:52 +0530

    Create p68.py

---
+++ p68.py
@@ -0,0 +1,10 @@
+x,y= input().split()
+if(x.isdigit() and y.isdigit()):
+    x=int(x)
+    y=int(y)
+    if(y==1):
+        print("1 2")
+    else:
+        print(str("1 "+str(x-y)))
+else:
+    print("invalid")
